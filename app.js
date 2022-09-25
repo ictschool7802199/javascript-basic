036654e4dcf34e0fd3e144cce9c5c84eca34585c
ictschool7802199 <113285540+ictschool7802199@users.noreply.github.com>
2022-09-25 17:50:25 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("ICT SCHOOL");
+
+var a = 5; // integer
+var b = "hello"; // string
+var c = true || false; // boolean
+var d = 5.3; // float
+
+if (a == 5) {
+  console.log("YES");
+} else {
+  console.log("No");
+}
+
+if (a === 5) {
+  console.log("YES");
+} else {
+  console.log("No");
+}
+
+// QUIZ
+// > 80 : A
+// 80 - 70 : B
+// 69 - 60 : C
+// 59 - 50 : D
+// < 50 : F
+
+var score = 70;
+
+if (score > 80) {
+  console.log("A");
+} else if (score >= 70) {
+  console.log("B");
+}

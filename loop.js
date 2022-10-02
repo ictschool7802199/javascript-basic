36b2482e39013824ad6710e1fa2e88d4af4a4fc9
ictschool7802199 <113285540+ictschool7802199@users.noreply.github.com>
2022-10-02 17:53:16 +0700

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,29 @@
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// let i = 0;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// sum 1 - 100
+// let lastNumber = 100;
+// let result = 0;
+
+// for (let i = 1; i <= lastNumber; i++) {
+//   result = result + i;
+// }
+// console.log(result);
+
+// let time = 2;
+// for (let i = 1; i <= 12; i++) {
+//   console.log(`${time} x ${i} = ${time * i}`);
+// }
+
+for (let x = 1; x <= 100; x++) {
+  if (x % 2 == 0) {
+    console.log(x);
+  }
+}
